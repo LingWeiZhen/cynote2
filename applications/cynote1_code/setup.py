5fac789f5d4f47af81fcec03a04554a09a1f507b
Maurice Ling <mauriceling@acm.org>
2016-01-09 10:12:03 +0000

    + extracted code archive of CyNote version 1.4.1 for conversion to CyNote 2. This extracted code archive is the same as in cynote1_code.bz2 (in case you screwed up this archive and needs to be replaced)

---
+++ applications/cynote1_code/setup.py
@@ -0,0 +1,36 @@
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(name='cynote',
+      version='1.1',
+      description='Cyber Laboratory Notebook',
+      long_description='Laboratory notebook using version control system for \
+      backups and independent date-time stamping (which may be a form of \
+      notarization), in order to ensure record accountability and auditing.',
+      author='Maurice HT Ling',
+      author_email='mauriceling@acm.org',
+      url='http://cynote.sourceforge.net',
+      download_url='http://sourceforge.net/projects/cynote/files/CyNote/ \
+      cynote1_1.zip/download',
+      license = 'GNU General Public License version 3',
+      platform = 'OS independent',
+      package_dir = {},
+      packages = [],
+      install_requires = ['biopython==1.50',
+                          'pil==1.1.6'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: End Users/Desktop',
+                   'Intended Audience :: Information Technology',
+                   'Intended Audience :: Science/Research',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: GNU General Public License \
+                   (GPL)',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Utilities'
+                    ],
+     )
